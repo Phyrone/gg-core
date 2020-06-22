90de16736aa0bb28a22bdddbb3c18f852f1462b0
Phyrone <phyrone@phyrone.de>
2020-06-22 17:45:54 +0200

    filter fastboard if deleted

--- gg-core-bukkit-common/src/main/kotlin/de/phyrone/gg/bukkit/utils/FastBoardUtils.kt
+++ gg-core-bukkit-common/src/main/kotlin/de/phyrone/gg/bukkit/utils/FastBoardUtils.kt
@@ -5,6 +5,7 @@ import org.bukkit.entity.Player
 import java.util.*
 
 val Player.fastBoard: FastBoard
-    get() = fastBardMap.getOrPut(this) { FastBoard(this) }
+    get() = fastBardMap[this]?.takeUnless { it.isDeleted } ?: FastBoard(this)
+        .also { fastBoard -> fastBardMap[this] = fastBoard }
 
 private val fastBardMap = WeakHashMap<Player, FastBoard>()
