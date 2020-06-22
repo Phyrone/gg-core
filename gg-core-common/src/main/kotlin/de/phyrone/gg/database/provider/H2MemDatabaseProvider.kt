34696188b42fde99ba95bbf8f93dcf8d35429c85
Phyrone <phyrone@phyrone.de>
2020-06-22 20:07:23 +0200

    lifetime of memory databases now equals lifetime of jvm

--- gg-core-common/src/main/kotlin/de/phyrone/gg/database/provider/H2MemDatabaseProvider.kt
+++ gg-core-common/src/main/kotlin/de/phyrone/gg/database/provider/H2MemDatabaseProvider.kt
@@ -1,17 +1,22 @@
 package de.phyrone.gg.database.provider
 
 import com.zaxxer.hikari.HikariConfig
+import de.phyrone.gg.KOIN_PLUGIN_NAME
 import de.phyrone.gg.common.config.Konf
 import org.koin.core.Koin
-import java.util.*
+import org.koin.core.qualifier.named
 
 object H2MemDatabaseProvider : AbstractHikariProvider() {
     override fun getURL(config: Konf, koin: Koin): String {
-        return "jdbc:h2:mem:${UUID.randomUUID()}"
+        return "jdbc:h2:mem:${koin.get<String>(named(KOIN_PLUGIN_NAME))};DB_CLOSE_DELAY=-1"
     }
 
     override fun HikariConfig.override() {
-        minimumIdle = 1
+        minimumIdle = 0
+        maximumPoolSize = Int.MAX_VALUE
+        idleTimeout = 1000
+        maxLifetime = Long.MAX_VALUE
+
     }
 
     override val names: Set<String> = setOf("h2-memory", "h2-mem", "mem", "memory")
