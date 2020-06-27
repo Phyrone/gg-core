f50f5a47d80f855beb7ce5a4406fa0a449b82c96
Phyrone <phyrone@phyrone.de>
2020-06-27 14:50:15 +0200

    add PagedHotbarTemplate.kt

---
+++ gg-core-bukkit-common/src/main/kotlin/de/phyrone/gg/bukkit/hotbar/template/PagedHotbarTemplate.kt
@@ -0,0 +1,136 @@
+package de.phyrone.gg.bukkit.hotbar.template
+
+import de.phyrone.gg.bukkit.hotbar.PlayerHotbar
+import de.phyrone.gg.bukkit.items.InteractiveItem
+import org.bukkit.entity.Player
+
+open class PagedHotbarTemplate private constructor(
+    private val pages: MutableList<Array<InteractiveItem?>>
+) : HotbarTemplate {
+    constructor() : this(ArrayList())
+
+
+    var currentPage = 0
+
+    override fun PlayerHotbar.append(player: Player) {
+        pages.getOrNull(currentPage)?.forEachIndexed { slot: Int, interactiveItem: InteractiveItem? ->
+            if (interactiveItem != null)
+                this.setItem(slot, interactiveItem)
+        }
+    }
+
+    fun setItem(slot: Int, item: InteractiveItem) {
+        if (slot < 0) return
+        val page = calculatePageNumber(slot)
+        val pagedSlot = calculatePagedSlot(slot, page)
+        setItem(page, pagedSlot, item)
+    }
+
+    fun setItem(page: Int, slot: Int, item: InteractiveItem) {
+        assert(page >= 0)
+        assert(slot in (0..8))
+        val pageArray = getPageOrCreate(page)
+        pageArray[slot] = item
+    }
+
+    fun removeItem(slot: Int) {
+        if (slot < 0) return
+        val page = calculatePageNumber(slot)
+        val pagedSlot = calculatePagedSlot(slot, page)
+        removeItem(page, pagedSlot)
+    }
+
+    fun removeItem(page: Int, slot: Int) {
+        assert(page >= 0)
+        assert(slot in (0..8))
+        pages.getOrNull(slot)?.set(slot, null)
+        collectOldPages()
+    }
+
+    private fun collectOldPages() {
+        var collectSize = 0
+        for (page in pages.reversed()) {
+            if (page.all { it == null }) collectSize++
+            else break
+        }
+        repeat(collectSize) {
+            pages.removeAt(pages.size - 1)
+        }
+    }
+
+    private fun getPageOrCreate(page: Int): Array<InteractiveItem?> {
+
+        val currentSize = pages.size - 1
+        if (currentSize < page) {
+            val pagesToCreate = page - currentSize
+            repeat(pagesToCreate) {
+                pages.add(Array(9) { null })
+            }
+        }
+        return pages[page]
+    }
+
+    @Suppress("NOTHING_TO_INLINE")
+    private inline fun calculatePageNumber(slot: Int) = slot / 9
+
+    @Suppress("NOTHING_TO_INLINE")
+    private inline fun calculatePagedSlot(slot: Int, page: Int) = slot - page * 9
+
+    @Suppress("NOTHING_TO_INLINE")
+    private inline fun isLast() = currentPage >= lastPage()
+
+    @Suppress("NOTHING_TO_INLINE")
+    private inline fun lastPage() = (pages.size - 1)
+
+    @Suppress("NOTHING_TO_INLINE")
+    private inline fun isFirst() = currentPage <= 0
+
+
+    final override fun PlayerHotbar.next(player: Player): Boolean {
+        return if (pages.size > 1) {
+            val oldPage = currentPage
+            val direction: Direction
+            if (isLast()) {
+                currentPage = 0
+                direction = Direction.FIRST
+            } else {
+                currentPage++
+                direction = Direction.NEXT
+            }
+            onPageChange(oldPage, currentPage, direction)
+            true
+        } else {
+            false
+        }
+    }
+
+    final override fun PlayerHotbar.prev(player: Player): Boolean {
+        return if (pages.size > 1) {
+            val oldPage = currentPage
+            val direction: Direction
+            if (isFirst()) {
+                currentPage = lastPage()
+                direction = Direction.LAST
+            } else {
+                currentPage--
+                direction = Direction.PREV
+            }
+            onPageChange(oldPage, currentPage, direction)
+            true
+        } else {
+            false
+        }
+    }
+
+    open fun onPageChange(oldPage: Int, newPage: Int, direction: Direction) {
+
+    }
+
+    override fun copy(): HotbarTemplate = PagedHotbarTemplate(pages.map { page -> page.copyOf() }.toMutableList())
+
+
+    enum class Direction {
+        NEXT, PREV, FIRST, LAST
+    }
+
+}
