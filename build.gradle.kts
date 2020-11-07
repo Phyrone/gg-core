407652b4ed4a872e9a5794eb193824e4b7bd0750
Phyrone <phyrone@phyrone.de>
2020-11-07 23:07:05 +0100

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,7 +12,7 @@ plugins {
     //id("org.jetbrains.dokka") version "1.4.10"
     id("com.github.johnrengelman.shadow") version "5.2.0"
     id("de.undercouch.download") version "1.2"
-    kotlin("jvm") version "1.4.0"
+    kotlin("jvm") version "1.4.10"
 
 }
 
